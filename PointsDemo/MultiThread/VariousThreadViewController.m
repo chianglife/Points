002489738f81cb6c28782fbd03b5a3c3b88c6f82
Chiang <1274921245@qq.com>
2022-06-30 09:35:54 +0800

    GCD

--- PointsDemo/MultiThread/VariousThreadViewController.m
+++ PointsDemo/MultiThread/VariousThreadViewController.m
@@ -16,7 +16,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"NSThread & GCD & NSOperation";
-    [self test_createNSTread];
+//    [self test_createNSTread];
+//    [self test_dispatch_apply];
+//    [self test_dispatch_group2];
+    [self test_dispatch_group_wait];
 }
 
 - (void)test_createNSTread {
@@ -51,4 +54,120 @@
     });
 }
 
+- (void)test_dispatch_apply{
+    /*
+     dispatch_apply将指定的Block追加到指定的队列中重复执行，并等到全部的处理执行结束——相当于线程安全的for循环
+
+     应用场景：用来拉取网络数据后提前算出各个控件的大小，防止绘制时计算，提高表单滑动流畅性
+     - 添加到串行队列中——按序执行
+     - 添加到主队列中——死锁
+     - 添加到并发队列中——乱序执行
+     - 添加到全局队列中——乱序执行
+     */
+    
+    dispatch_queue_t queue = dispatch_queue_create("Chiang", DISPATCH_QUEUE_CONCURRENT);
+    NSLog(@"dispatch_apply前");
+    /**
+         param1：重复次数
+         param2：追加的队列
+         param3：执行任务
+         */
+    dispatch_apply(10, queue, ^(size_t index) {
+        NSLog(@"dispatch_apply 的线程 %zu - %@", index, [NSThread currentThread]);
+    });
+    NSLog(@"dispatch_apply后");
+}
+
+
+- (void)test_dispatch_group1{
+    /*
+     dispatch_group_t：调度组将任务分组执行，能监听任务组完成，并设置等待时间
+
+     应用场景：多个接口请求之后刷新页面
+     */
+    
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"请求一完成");
+    });
+    
+    dispatch_group_async(group, queue, ^{
+        NSLog(@"请求二完成");
+    });
+    
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        NSLog(@"刷新页面");
+    });
+}
+
+- (void)test_dispatch_group2{
+    /*
+     dispatch_group_enter和dispatch_group_leave成对出现，使进出组的逻辑更加清晰
+     */
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    
+    dispatch_group_enter(group);
+    dispatch_async(queue, ^{
+        NSLog(@"请求一完成");
+        dispatch_group_leave(group);
+    });
+    
+    dispatch_group_enter(group);
+    dispatch_async(queue, ^{
+        NSLog(@"请求二完成");
+        dispatch_group_leave(group);
+    });
+    
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        NSLog(@"刷新界面");
+    });
+}
+
+
+- (void)test_dispatch_group_wait{
+    /*
+     long dispatch_group_wait(dispatch_group_t group, dispatch_time_t timeout)
+
+     group：需要等待的调度组
+     timeout：等待的超时时间（即等多久）
+        - 设置为DISPATCH_TIME_NOW意味着不等待直接判定调度组是否执行完毕
+        - 设置为DISPATCH_TIME_FOREVER则会阻塞当前调度组，直到调度组执行完毕
+
+
+     返回值：为long类型
+        - 返回值为0——在指定时间内调度组完成了任务
+        - 返回值不为0——在指定时间内调度组没有按时完成任务
+
+     */
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    
+    dispatch_group_enter(group);
+    dispatch_async(queue, ^{
+        NSLog(@"请求一完成");
+        dispatch_group_leave(group);
+    });
+    
+    dispatch_group_enter(group);
+    dispatch_async(queue, ^{
+        NSLog(@"请求二完成");
+        dispatch_group_leave(group);
+    });
+//    long timeout = dispatch_group_wait(group, DISPATCH_TIME_NOW);
+//    long timeout = dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+    long timeout = dispatch_group_wait(group, dispatch_time(DISPATCH_TIME_NOW, 1 *NSEC_PER_SEC));
+    NSLog(@"timeout = %ld", timeout);
+    if (timeout == 0) {
+        NSLog(@"按时完成任务");
+    }else{
+        NSLog(@"超时");
+    }
+    
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        NSLog(@"刷新界面");
+    });
+}
 @end
